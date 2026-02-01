7e4bda91e97a22f103597f32b1e953eaa95f2420
marcsello <punkosdmarcell@rocketmail.com>
2026-02-01 18:24:10 +0100

    add workaround for cs2

--- cmd/agent/scrapers/valve/valve.go
+++ cmd/agent/scrapers/valve/valve.go
@@ -12,7 +12,8 @@ import (
 )
 
 type ScraperConfig struct {
-	Address string `mapstructure:"address" default:"127.0.0.1"`
+	Address       string `mapstructure:"address" default:"127.0.0.1"`
+	WorkaroundCS2 bool   `mapstructure:"workaround_cs2" default:"false"` // THIS ALSO NEEDS THIS TO BE INSTALLED ON THE SERVER: https://github.com/Source2ZE/ServerListPlayersFix !!!
 }
 
 type Scraper struct {
@@ -53,6 +54,19 @@ func (s Scraper) Scrape(ctx context.Context, logger *zap.Logger) (model.GameServ
 		logger.Warn("Failed to query players. Continuing...", zap.Error(err))
 		players = nil
 	}
+
+	if s.config.WorkaroundCS2 { // CS2 has this weird thing, that upon startup it's showing two players with no name (if name fix is installed)
+		if info.Players == 2 && players != nil && len(players.Players) == 2 && players.Count == 2 &&
+			players.Players[0].Name == "" && players.Players[1].Name == "" && players.Players[0].Score == 0 &&
+			players.Players[1].Score == 0 {
+			// Yup we are in that weird state again...
+			// just alter the response we got...
+			players = nil
+			info.Players = 0
+		}
+	}
+
+	// Convert data, then send it back
 
 	var onlinePlayers []model.Player
 	if players != nil {
